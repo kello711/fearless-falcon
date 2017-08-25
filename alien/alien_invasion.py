c74b887833454a7f9aacebe0ba7189661ceeef6a
Kelly Lehman <kello711@gmail.com>
2017-08-25 18:59:01 -0500

    Finished chapter 12 of Python Crash Course

--- alien/alien_invasion.py
+++ alien/alien_invasion.py
@@ -1,9 +1,9 @@
-import sys
-
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	# Initialize pygame, settings, and create a screen object
@@ -17,20 +17,16 @@ def run_game():
 	bg_color = (230, 230, 230)
 
 	# Make a ship
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
+
+	# Make a group to store bullets in
+	bullets = Group()
 
 	# Start the main game loop
 	while True:
-		# Watch for keyboard and mouse events
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-
-		# Redraw the screen during each pass through the loop
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
-
-		# Make the most recently drawn screen visible
-		pygame.display.flip()
+		gf.check_events(ai_settings, screen, ship, bullets)
+		ship.update()
+		gf.update_bullets(bullets)
+		gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- alien/settings.py
+++ alien/settings.py
@@ -7,3 +7,13 @@ class Settings():
 		self.screen_width = 1200
 		self.screen_height = 800
 		self.bg_color = (230, 230, 230)
+
+		# SHip Settings
+		self.ship_speed_factor = 4.5
+
+		# Bullet Settings
+		self.bullet_speed_factor = 10
+		self.bullet_width = 3
+		self.bullet_height = 15
+		self.bullet_color = (60, 60, 60)
+		self.bullets_allowed = 3
