84a644800f6728cd768ef12a75c5048772741be8
kello711 <kello711@gmail.com>
2017-10-24 14:21:23 -0500

    Working chapter 16.

---
+++ python_work/mpl_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+input_values = []
+squares = []
+for x in range(1,6):
+    input_values.append(x)
+    squares.append(x**2)
+
+# squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares, linewidth = 5)
+
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize = 24)
+plt.xlabel("Value", fontsize = 14)
+plt.ylabel("Square of Value", fontsize = 14)
+
+# Set size of tick labels.
+plt.tick_params(axis = 'both', labelsize = 14)
+
+plt.show()
---
+++ python_work/rw_visual.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Keep making new walks, as long as the program is active
+while True:
+    # Make random walk, and plot the points
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s = 15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
